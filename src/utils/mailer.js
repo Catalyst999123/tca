48c074cfccf985dc205b85d2648d2493855d6549
Bruno Kiafuka <brunokiafuka@live.com.pt>
2022-07-11 21:37:43 +0100

    ✨ fixed mailer function

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,9 +1,9 @@
 
 // eslint-disable-next-line no-restricted-globals
-export const baseURL = location.hostname === 'localhost' ? 'http://localhost:8888/.netlify/functions' : `${location.protocol}/${location.host}/.netlify/functions`
+export const baseURL = location.hostname === 'localhost' ? 'http://localhost:8888/.netlify/functions' : `${location.protocol}//${location.host}/.netlify/functions`
 
 export const mailer = async (subject, text) => {
-    console.log({ baseURL })
+
     try {
         await fetch(`${baseURL}/sendMail`, {
             method: 'POST',
